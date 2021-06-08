6c93de750130c8771a22288bb28c09bbaf3caa36
“DaubieMatthieu” <48768232+DaubieMatthieu@users.noreply.github.com>
2021-06-08 15:12:28 +0200

    DIS-27: Initialize Spring Application

---
+++ src/main/java/isep/gl/distouch/DistouchApplication.java
@@ -0,0 +1,13 @@
+package isep.gl.distouch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistouchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DistouchApplication.class, args);
+    }
+
+}
---
+++ src/test/java/isep/gl/distouch/DistouchApplicationTests.java
@@ -0,0 +1,13 @@
+package isep.gl.distouch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DistouchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
