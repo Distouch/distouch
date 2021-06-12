264c4b9e24431d88dc7bcaf454ec82b1603e6fc5
“DaubieMatthieu” <48768232+DaubieMatthieu@users.noreply.github.com>
2021-06-12 20:37:02 +0200

    DIS-30: Add registration page

--- src/main/java/isep/gl/distouch/controller/LoginController.java
+++ src/main/java/isep/gl/distouch/controller/LoginController.java
@@ -1,12 +1,23 @@
 package isep.gl.distouch.controller;
 
+import isep.gl.distouch.model.User;
+import isep.gl.distouch.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.validation.Valid;
+
 @Controller
 public class LoginController {
+    @Autowired
+    private UserService userService;
+
     @RequestMapping("/login")
     public String loginPage(Model model,
                             @RequestParam(value = "logout", required = false) boolean logout,
@@ -20,5 +31,26 @@ public class LoginController {
             model.addAttribute("messageType", "success");
         }
         return "login";
+    }
+
+    @GetMapping("/registration")
+    public String registrationPage(Model model) {
+        model.addAttribute("user", new User());
+        return "/registration";
+    }
+
+    @PostMapping("/registration")
+    public String registerUser(@Valid User user, BindingResult bindingResult) {
+        User userExists = userService.findUserByEmail(user.getEmail());
+        if (userExists != null) {
+            bindingResult
+                    .rejectValue("email", "error.user",
+                            "There is already a user registered with the email provided");
+        }
+        if (!bindingResult.hasErrors()) {
+            userService.saveUser(user);
+            return "redirect:/login";
+        }
+        return "/registration";
     }
 }
